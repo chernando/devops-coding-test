0849430e9e153aa1287c1778f156ba1287cd3283
Carlos Hernando Carasol <301211+chernando@users.noreply.github.com>
2023-07-16 13:17:10 +0200

    build: configure build & run scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,27 @@
 #!/bin/sh
+#
+set -e
 
-# TODO: implement
+cd "$(dirname "$0")/.."
+
+echo "🏗  Build started at… $(date "+%H:%M:%S")"
+./mvnw clean install
+
+. target/maven-archiver/pom.properties
+JAR_FILE=target/$artifactId-$version.jar
+echo "🐳  Build docker for $JAR_FILE at… $(date "+%H:%M:%S")"
+
+if test -f "$JAR_FILE"; then
+
+	docker build \
+		--file scripts/Dockerfile \
+		--tag example/$artifactId:$version \
+		--build-arg jar_file=$JAR_FILE \
+		.
+
+else
+	echo "🛑  $JAR_FILE not found"
+	exit 1
+fi
+
+echo "✅  All done! $(date "+%H:%M:%S")"
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,3 +1,22 @@
 #!/bin/sh
 
-# TODO: implement
+set -e
+
+cd "$(dirname "$0")/.."
+
+if test -f target/maven-archiver/pom.properties; then
+	echo "▶️  Started at… $(date "+%H:%M:%S")"
+	. target/maven-archiver/pom.properties
+
+	docker run \
+		-p 8443:8443 \
+		--interactive \
+		--tty \
+		--rm \
+		example/$artifactId:$version
+	echo "⏹  Stopped at… $(date "+%H:%M:%S")"
+
+else
+	echo "🛑  You need to use 'scripts/build.sh' first"
+	exit 1
+fi
